260ae4864eb3eee8492bc95371c9adf3f228a3c0
Sheldon Hancock <me@something.com>
2017-10-13 10:42:38 -0600

    Unit tests and code for AllowedAcrossAtATime

---
+++ Monkeys/Monkeys/Exceptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monkeys
+{
+    public class NumberLessThanOneException : Exception
+    {
+        public NumberLessThanOneException() : base() { }
+        public NumberLessThanOneException(string message) : base(message) { }
+        public NumberLessThanOneException(string message, Exception innerException) : base(message, innerException) { }
+        public NumberLessThanOneException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
