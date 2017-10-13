9ba97092e940577334ae8bee875d26a642d75732
Sheldon Hancock <me@something.com>
2017-10-13 11:18:22 -0600

    Added a class to represent a list of monkeys

---
+++ Monkeys/Monkeys/MonkeyLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monkeys
+{
+    class MonkeyLine
+    {
+        public string Name { get; set; }
+        public int Count { get { return Monkeys.Count; } }
+
+        private List<Monkey> _monkeys = new List<Monkey>();
+        public List<Monkey> Monkeys
+        {
+            get { return _monkeys; }
+            set { _monkeys = value; }
+        }
+
+    }
+}
