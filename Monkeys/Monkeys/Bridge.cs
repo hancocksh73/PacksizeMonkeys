6c960db9e4170baa4b13c63edb8435cac94db113
Sheldon Hancock <me@something.com>
2017-10-13 10:21:36 -0600

    unit tests for crossing left-to-right (again)

--- Monkeys/Monkeys/Bridge.cs
+++ Monkeys/Monkeys/Bridge.cs
@@ -4,6 +4,16 @@ namespace Monkeys
 {
     internal class Bridge
     {
+        private int _count;
+
         public List<Monkey> LeftStart { get; set; }
+        public List<Monkey> RightStart { get; set; }
+        public List<Monkey> RightEnd { get; private set; }
+        public List<Monkey> LeftEnd { get; private set; }
+
+        public void CrossTheBridge(List<Monkey> leftStart, List<Monkey> rightStart)
+        {
+
+        }
     }
 }
--- Monkeys/UnitTestProject1/BridgeTest.cs
+++ Monkeys/UnitTestProject1/BridgeTest.cs
@@ -22,6 +22,52 @@ namespace MonkeyTests
             };
 
             Assert.AreEqual(3, bridge.LeftStart.Count);
+            Assert.AreEqual("Brown", bridge.LeftStart[0].Name);
+            Assert.AreEqual("Yiper", bridge.LeftStart[1].Name);
+            Assert.AreEqual("Smive", bridge.LeftStart[2].Name);
+        }
+
+        [TestMethod]
+        public void RightStartHasMonkeys()
+        {
+            Bridge bridge = new Bridge
+            {
+                LeftStart = new List<Monkey>
+                {
+                    new Monkey { Id = 1, Name = "Green" },
+                    new Monkey { Id = 2, Name = "Albert" },
+                    new Monkey { Id = 3, Name = "Sylie" }
+                }
+            };
+
+            Assert.AreEqual(3, bridge.LeftStart.Count);
+            Assert.AreEqual("Green", bridge.LeftStart[0].Name);
+            Assert.AreEqual("Albert", bridge.LeftStart[1].Name);
+            Assert.AreEqual("Sylie", bridge.LeftStart[2].Name);
+        }
+
+        [TestMethod]
+        public void CrossLeftMonkeys()
+        {
+            Bridge bridge = new Bridge
+            {
+                LeftStart = new List<Monkey>
+                {
+                    new Monkey { Id = 1, Name = "Brown" },
+                    new Monkey { Id = 2, Name = "Yiper" },
+                    new Monkey { Id = 3, Name = "Smive" }
+                }
+            };
+
+            Assert.AreEqual(3, bridge.LeftStart.Count);
+
+            bridge.CrossTheBridge(bridge.LeftStart, bridge.RightStart);
+
+            Assert.AreEqual(0, bridge.LeftStart.Count);
+            Assert.AreEqual(3, bridge.RightEnd.Count);
+            Assert.AreEqual("Brown", bridge.RightEnd[0].Name);
+            Assert.AreEqual("Yiper", bridge.RightEnd[1].Name);
+            Assert.AreEqual("Smive", bridge.RightEnd[2].Name);
         }
     }
 }
